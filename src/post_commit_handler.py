f53e988e648d1435e17881b9154e17551f75a01e
Dave Rooney <daverooneyca@gmail.com>
2022-01-28 14:00:15 -0500

    Fixed missed self reference after refactor

--- src/post_commit_handler.py
+++ src/post_commit_handler.py
@@ -5,6 +5,7 @@ from commit_details_builder import CommitDetailsBuilder
 
 class PostCommitHandler():
   def __init__(self, rating_file="./rating_file", destination_url="http://127.0.0.1:5000/commit"):
+    
     # TODO - use a configuration file for these settings
     self.rating_file = rating_file
     self.destination_url = destination_url
