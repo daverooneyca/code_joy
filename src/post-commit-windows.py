32775b12158477041a79c9e6d6d295cca1aeef00
Dave Rooney <daverooneyca@gmail.com>
2022-01-28 15:42:41 -0500

    Updated the Windows version of the post_commit hook

--- src/post-commit-windows.py
+++ src/post-commit-windows.py
@@ -1,8 +1,11 @@
 #!%userprofile%\AppData\Local\Microsoft\WindowsApps\python.exe
 
 from post_commit_handler import PostCommitHandler
+from http_commit_repository import HttpCommitRepository
 
 if __name__ == '__main__':
-   handler = PostCommitHandler()
+   repository = HttpCommitRepository()
+
+   handler = PostCommitHandler(repository)
    
    handler.execute()
