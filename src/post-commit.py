6a725dabbcf58acc0c4120b7f8c270ef1dee6133
Dave Rooney <daverooneyca@gmail.com>
2022-01-28 15:02:58 -0500

    Fixed missing import

--- src/post-commit.py
+++ src/post-commit.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from post_commit_handler import PostCommitHandler
+from http_commit_repository import HttpCommitRepository
 
 if __name__ == '__main__':
    repository = HttpCommitRepository(self.destination_url)
