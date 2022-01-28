1755b378dfe6799691aeb79f83ee5f93c0cb2831
Dave Rooney <daverooneyca@gmail.com>
2022-01-28 15:05:09 -0500

    Fixed incorrect self reference

--- src/post-commit.py
+++ src/post-commit.py
@@ -4,7 +4,7 @@ from post_commit_handler import PostCommitHandler
 from http_commit_repository import HttpCommitRepository
 
 if __name__ == '__main__':
-   repository = HttpCommitRepository(self.destination_url)
+   repository = HttpCommitRepository()
 
    handler = PostCommitHandler(repository)
    
